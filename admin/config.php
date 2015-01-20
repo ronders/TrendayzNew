19052ee1074c500d7b8ce4d7ffbede9a2e981447
Ron Dyrcz <ron@port80corp.com>
2015-01-20 14:47:28 -0600

    Install of Opencart

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:7888/Trendayz/admin/');
+define('HTTP_CATALOG', 'http://localhost:7888/Trendayz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:7888/Trendayz/admin/');
+define('HTTPS_CATALOG', 'http://localhost:7888/Trendayz/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Ronders/Projects/Trendayz/admin/');
+define('DIR_SYSTEM', '/Users/Ronders/Projects/Trendayz/system/');
+define('DIR_DATABASE', '/Users/Ronders/Projects/Trendayz/system/database/');
+define('DIR_LANGUAGE', '/Users/Ronders/Projects/Trendayz/admin/language/');
+define('DIR_TEMPLATE', '/Users/Ronders/Projects/Trendayz/admin/view/template/');
+define('DIR_CONFIG', '/Users/Ronders/Projects/Trendayz/system/config/');
+define('DIR_IMAGE', '/Users/Ronders/Projects/Trendayz/image/');
+define('DIR_CACHE', '/Users/Ronders/Projects/Trendayz/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Ronders/Projects/Trendayz/download/');
+define('DIR_LOGS', '/Users/Ronders/Projects/Trendayz/system/logs/');
+define('DIR_CATALOG', '/Users/Ronders/Projects/Trendayz/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'mysql');
+define('DB_PREFIX', 'tz2_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:7888/Trendayz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:7888/Trendayz/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Ronders/Projects/Trendayz/catalog/');
+define('DIR_SYSTEM', '/Users/Ronders/Projects/Trendayz/system/');
+define('DIR_DATABASE', '/Users/Ronders/Projects/Trendayz/system/database/');
+define('DIR_LANGUAGE', '/Users/Ronders/Projects/Trendayz/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Ronders/Projects/Trendayz/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Ronders/Projects/Trendayz/system/config/');
+define('DIR_IMAGE', '/Users/Ronders/Projects/Trendayz/image/');
+define('DIR_CACHE', '/Users/Ronders/Projects/Trendayz/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Ronders/Projects/Trendayz/download/');
+define('DIR_LOGS', '/Users/Ronders/Projects/Trendayz/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'mysql');
+define('DB_PREFIX', 'tz2_');
+?>
