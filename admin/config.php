554423506bcb39bbf17214146b5d781bce782409
Ron Dyrcz <ron@port80corp.com>
2015-01-23 16:17:17 -0600

    Change of DB
    Changed DB from local to dreamiest

--- admin/config.php
+++ admin/config.php
@@ -22,9 +22,9 @@ define('DIR_CATALOG', '/Users/Ronders/Projects/Trendayz/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'mysql');
+define('DB_HOSTNAME', 'mysql.dev.trendayz.com');
+define('DB_USERNAME', 'adyrcz');
+define('DB_PASSWORD', 'Achi11e5');
+define('DB_DATABASE', 'db_trendayz_dev');
 define('DB_PREFIX', 'tz2_');
 ?>
