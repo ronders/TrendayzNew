bb08f96369284db7c6a132b34c79c1c53f4d300a
Ron Dyrcz <ron@port80corp.com>
2015-01-27 15:08:18 -0600

    mysql DB change

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ define('HTTP_SERVER', 'http://localhost:7888/Trendayz/');
 // HTTPS
 define('HTTPS_SERVER', 'http://localhost:7888/Trendayz/');
 
+
 // DIR
 define('DIR_APPLICATION', '/Users/Ronders/Projects/Trendayz/catalog/');
 define('DIR_SYSTEM', '/Users/Ronders/Projects/Trendayz/system/');
@@ -19,9 +20,9 @@ define('DIR_LOGS', '/Users/Ronders/Projects/Trendayz/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'mysql');
+define('DB_HOSTNAME', 'mysql.dev.trendayz.com');
+define('DB_USERNAME', 'adyrcz');
+define('DB_PASSWORD', 'Achi11e5');
+define('DB_DATABASE', 'db_trendayz_dev');
 define('DB_PREFIX', 'tz2_');
 ?>
